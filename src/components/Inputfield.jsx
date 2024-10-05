cfeedb675e10f99eb84f20292fe943ab43a97e06
SudhanshuDevlpr <singhsudhanshu5656@yahoo.com>
2024-10-04 23:43:14 -0700

    create dist file

--- src/components/Inputfield.jsx
+++ src/components/Inputfield.jsx
@@ -1,11 +1,11 @@
-function Inputfield({onkeyup})
+function Inputfield({onkeyUp})
 {
 
     return<>
     
         <div className="row d-flex justify-content-center">
             <div className="col-12" >
-                    <input className="form-control border-3 border-primary " onKeyDown={onkeyup} type="text" placeholder="Enter Healthy Food Item" aria-label=".form-control-lg example"/>
+                    <input className="form-control border-3 border-primary " onKeyDown={onkeyUp} type="text" placeholder="Enter Healthy Food Item" aria-label=".form-control-lg example"/>
             </div>
     </div>
     </>
